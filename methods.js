52dfc94412ceb6f6e08112f892a462e3b0784a5f
Sooraj Shukla <84338880+Soorajshukla01@users.noreply.github.com>
2024-09-06 10:23:55 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,23 @@
+// The code below is about Methods in Javascript.
+let sooraj = ["Hero","Villian", 2,"Asus"]
+let b = sooraj.join(" and ") // Joining the array elements.
+console.log(b)
+let c = sooraj.toString() //To convert the array from numbers to strings.
+console.log(c)
+console.log(typeof c)
+let d = sooraj.pop() // To delete the last element.
+console.log(d)
+console.log(sooraj)
+let e = sooraj.push("50") // To last new last element.
+console.log(sooraj)
+let f = sooraj.shift() // To delete the first element and shift other elements to the lower index value.
+console.log(sooraj)
+let g = sooraj.unshift("Chief Minsiter") //Add new element in the array at the beginning.
+console.log(sooraj)
+let neha = ["Deputy CM"," Heroine", 1, "Lenovo"]
+let h = sooraj.concat(neha)// Add Two Different Array Elements.
+console.log(h)
+// The Reflexive of the unpopular in the context of the arranged in the height of teh subconcious in the rent in the contexctual in the parameter of the rent in the context of the demonice in the parameter. The Upvocing in the context if teh managed in the rent of teh rentfree classes in the obsolute of the very minuses in the context of the rent free duties. 
+
+
+
