22b02521e3648b14238d11db3e5ab9c7cc6e0b93
Sooraj Shukla <84338880+Soorajshukla01@users.noreply.github.com>
2024-09-07 22:07:27 +0530

    Add files via upload

---
+++ methods.js
@@ -0,0 +1,23 @@
+// The code below is about Methods in Javascript.
+let sooraj = ["Hero","Villian", 2,"Asus"]
+let b = sooraj.join(" and ") // Joining the array elements.
+console.log(b)
+let c = sooraj.toString() //To convert the array from numbers to strings.
+console.log(c)
+console.log(typeof c)
+let d = sooraj.pop() // To delete the last element.
+console.log(d)
+console.log(sooraj)
+let e = sooraj.push("50") // To add new last element.
+console.log(sooraj)
+let f = sooraj.shift() // To delete the first element and shift other elements to the lower index value.
+console.log(sooraj)
+let g = sooraj.unshift("Chief Minsiter") //Add new element in the array at the beginning.
+console.log(sooraj)
+let neha = ["Deputy CM"," Heroine", 1, "Lenovo"]
+let h = sooraj.concat(neha)// Add Two Different Array Elements.
+console.log(h)
+ 
+
+
+
