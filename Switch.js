e3354a7f07083f40fc60c42c3d137483ca56c562
Sooraj Shukla <84338880+Soorajshukla01@users.noreply.github.com>
2024-09-07 22:10:00 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,21 @@
+let day = "Friday";
+switch (day){
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "Thursday":
+        console.log("Today is Wednesday");
+        break;
+    case "Friday":
+        console.log("Today is Friday");
+        break;
+    case "Saturday":
+        console.log("Today is Saturday");
+        break;
+    case "Sunday":
+        console.log("Today is Sunday");
+}
+let 
